eaa2030389cfd100ef04aa17a7f13d4f8f6bcd38
Ken <dagacayken@gmail.com>
2025-07-30 15:35:24 +0800

    Finish connect app to firebase -> Custom Auth -> firestore

---
+++ app/src/main/java/com/fak/classmate/model/AuthViewModel.kt
@@ -0,0 +1,14 @@
+package com.fak.classmate.model
+
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel : ViewModel() {
+    fun login() {
+
+    }
+
+    fun signup() {
+
+    }
+
+}
---
+++ app/src/main/java/com/fak/classmate/model/UserData.kt
@@ -0,0 +1,7 @@
+package com.fak.classmate.model
+
+data class UserData(
+    var firstname: String = "",
+    var lastname: String = "",
+    var email: String = ""
+)
